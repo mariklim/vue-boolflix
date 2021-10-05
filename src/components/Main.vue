c6bec99a592fb9350e4d2aec374d67dca83292b1
mariklim <maria.klimenko889@gmail.com>
2021-10-05 16:48:11 +0200

    add Axios and Api

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,29 @@
 <template>
-<section>
-
-</section>
+  <section></section>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-    name: "Main"
-
-}
+  name: "Main",
+  props: {
+    infoFilm: String,
+  },
+  created() {
+    axios
+      .get('https://api.themoviedb.org/3/search/movie', {
+        params: {
+          api_key: '49c4ea87380c56e0d9c16d3ec78d73d7',
+          query: 'Ritorno al futuro',
+          language: 'it-IT',
+        },
+      })
+      .then(function (response) {
+        console.log(response);
+      });
+  },
+};
 </script>
 
 <style>
-
 </style>
