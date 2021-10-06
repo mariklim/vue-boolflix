983a31896d061e656f9be491829962dd49a31001
mariklim <maria.klimenko889@gmail.com>
2021-10-06 11:28:31 +0200

    add API for TV

--- src/App.vue
+++ src/App.vue
@@ -9,49 +9,65 @@
 </template>
 
 <script>
-import axios from 'axios';
-import Header from './components/Header.vue';
-import Main from './components/Main.vue';
-
+import axios from "axios";
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
-    Main
+    Main,
   },
   data() {
-		return {
+    return {
       //creo variabli vuoti dove salvare dati in arrivo: della input, e da API
-			inputText:"",
       allFilms: [],
-		}
-	},
+      allSeriesTv: [],
+    };
+  },
   methods: {
     //Al evento click avvio funzione "searchFilm", che "salva" il valore della input (quello che inserisce utente nella serachbar)
-		searchFilm(text) {
-			this.inputText = text;
-      console.log(text);
+    searchFilm(inputText) {
+      console.log(inputText);
       //al click si avvia la chiamata ad API e cambia il contenuto del Array allFilms secondo al valore della Query (che prende valore della input, quindi è dinamica, non serve fare un ulteriore filtro)
+      
+        // API FILMS
       axios
-      .get("https://api.themoviedb.org/3/search/movie", {
-        params: {
-          api_key: "49c4ea87380c56e0d9c16d3ec78d73d7",
-          
-          //faccio queri dinamica, il suo valore è quello che scrive utente nel input della searchbar
-          query: text,
-          language: "it-IT",
-        },
-      })
-      .then((response) => {
-        console.log(response.data.results);
-        //assegno direttamente all array tutto il contenuto da API, non serve fare il filtro
-        this.allFilms = response.data.results;
-        console.log(this.allFilms);
-      });
-		}
-	},
-}
+        .get("https://api.themoviedb.org/3/search/movie", {
+          params: {
+            api_key: "49c4ea87380c56e0d9c16d3ec78d73d7",
+
+            //faccio query dinamica, il suo valore è quello che scrive utente nel input della searchbar
+            query: inputText,
+            language: "it-IT",
+          },
+        })
+
+        // API SERIE TV
+        axios
+        .get("https://api.themoviedb.org/3/search/tv", {
+          params: {
+            api_key: "49c4ea87380c56e0d9c16d3ec78d73d7",
+
+            //faccio query dinamica, il suo valore è quello che scrive utente nel input della searchbar
+            query: inputText,
+            language: "it-IT",
+          },
+        })
+
+        .then((response) => {
+          console.log(response.data.results);
+          //assegno direttamente all array tutto il contenuto da API, non serve fare il filtro
+          this.allFilms = response.data.results;
+          // console.log(this.allFilms);
+
+          this.allSeriesTv = response.data.results;
+          console.log(this.allSeriesTv);
+        });
+    },
+  },
+};
 </script>
 
 <style lang="scss">
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,24 @@
 <template>
-<header>
-    <input type="text" v-model="inputText">
+  <header>
+    <input type="text" v-model="inputText" />
 
     <!-- al click mando dati della input al padre App -->
-    <button type="submit"  @click.prevent="$emit('search', inputText)">Search</button>
-
-</header>
+    <button type="submit" @click.prevent="$emit('search', inputText)">
+      Search
+    </button>
+  </header>
 </template>
 
 <script>
 export default {
-    name: "Header"
-
-}
+  name: "Header",
+  data() {
+    return {
+      inputText: "",
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
