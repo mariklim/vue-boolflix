7a5460854e7a02a6dafd12505138161ced0dade8
mariklim <maria.klimenko889@gmail.com>
2021-10-06 17:00:39 +0200

    add calc for vote int

--- src/components/filmCard.vue
+++ src/components/filmCard.vue
@@ -2,21 +2,21 @@
   <div>
     <!-- "info" è un singolo oggetto della array con i film, contiene tante info che ci servono -->
     <div class="card">
-        <h2 v-if="info.original_title">Titolo originale: {{ info.original_title }}
-        </h2>
-         <h2 v-else> Titolo originale: {{info.original_name }}
-        </h2>
-         <h2 v-if="info.title">Titolo: {{ info.title }}
-        </h2>
-         <h2 v-else> Titolo: {{info.name }}
-        </h2>
-        <h4>Voto: {{ info.vote_average }}</h4>
-        <h4>Lingua: </h4>
-        <lang-flag :iso="info.original_language" />
-        <img :src="(`https://image.tmdb.org/t/p/w342/${ info.poster_path }`)" alt="">
+      <h2 v-if="info.original_title">
+        Titolo originale: {{ info.original_title }}
+      </h2>
+      <h2 v-else>Titolo originale: {{ info.original_name }}</h2>
+      <h2 v-if="info.title">Titolo: {{ info.title }}</h2>
+      <h2 v-else>Titolo: {{ info.name }}</h2>
+      <h4>Voto: {{votoCalc}}</h4>
+      <h4>Lingua:</h4>
+      <lang-flag :iso="info.original_language" />
+      <img
+        :src="`https://image.tmdb.org/t/p/w342/${info.poster_path}`"
+        alt=""
+      />
     </div>
   </div>
-  
 </template> 
 
 <script>
@@ -30,6 +30,13 @@ export default {
   props: {
     info: Object,
   },
+  data(){
+      return{
+          votoCalc: parseInt(this.info.vote_average/2),
+      }
+  },
+       
+  
 };
 </script>
 
