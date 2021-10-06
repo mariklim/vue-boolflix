161a3a8d04a56f2dfd831135e5c81b9ffe9b0171
mariklim <maria.klimenko889@gmail.com>
2021-10-06 21:58:24 +0200

    add header style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,20 @@
 <template>
   <header>
     <div class="logo">
-      <img src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg" alt="logo  ">
+      <img
+        src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg"
+        alt="logo"
+      />
     </div>
 
     <div class="nav">
-    <input type="text" v-model="inputText" />
-    <!-- al click mando dati della input al padre App -->
-    <button type="submit" @click.prevent="$emit('search', inputText)">
-      Search
-    </button>
+      <div class="searchbar">
+        <input type="text" v-model="inputText" placeholder="cerca film e serial TV" />
+        <!-- al click mando dati della input al padre App -->
+        <button type="submit" @click.prevent="$emit('search', inputText)">
+          Search
+        </button>
+      </div>
     </div>
   </header>
 </template>
@@ -26,24 +31,37 @@ export default {
 </script>
 
 <style lang="scss">
-@import "../assets/style/variables.scss";
-@import "../assets/style/common.scss";
-header{
+header {
   display: flex;
   height: 4.375rem;
-  overflow: hidden; 
-  position: fixed; 
-  top: 0; 
+  overflow: hidden;
+  position: fixed;
+  top: 0;
   width: 100%;
 }
-.logo .nav{
-  border: 1px solid white;
-}
-.logo{
+
+.logo {
   width: 6.25rem;
-    img{
-      width: 100%;
-    }
+  img {
+    width: 100%;
+  }
 }
 
+.nav{
+  text-align: right;
+  width: 80%;
+    .searchbar{
+      display: inline-block;
+
+        & input{
+          width: 15.625rem;
+          height: 2.1875rem;
+          margin: 0 .625rem;
+          background-color: black;
+          color: white;
+          border: 1px solid white;
+          border-radius: 0;
+        }
+    }
+}
 </style>
