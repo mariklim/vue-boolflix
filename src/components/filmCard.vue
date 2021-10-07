e41d42210ee692fe51d51a53ee6fc56a2d99c706
mariklim <maria.klimenko889@gmail.com>
2021-10-07 17:51:19 +0200

    correct hover

--- src/components/filmCard.vue
+++ src/components/filmCard.vue
@@ -1,42 +1,48 @@
 <template>
-  
-    <!-- "info" è un singolo oggetto della array con i film, contiene tante info che ci servono -->
-    <div class="card">  
-      <!--Poster -->
-      <div class="card-img">
-        <img v-if="info.poster_path == null" :src="require(`../assets/image/netflix.jpg`)" alt="netflix">
-         <img v-else
-        :src="`https://image.tmdb.org/t/p/w200/${info.poster_path}`" alt=""/>
+  <!-- "info" è un singolo oggetto della array con i film, contiene tante info che ci servono -->
+  <div class="card">
+    <!--Poster -->
+    <div class="card-img">
+      <img
+        v-if="info.poster_path == null"
+        :src="require(`../assets/image/netflix.jpg`)"
+        alt="netflix"
+      />
+      <img
+        v-else
+        :src="`https://image.tmdb.org/t/p/w200/${info.poster_path}`"
+        alt=""
+      />
+
+      <div class="info-card">
+        <h3 v-if="info.original_title">
+          Titolo originale: {{ info.original_title }}
+        </h3>
+        <h3 v-else>Titolo originale: {{ info.original_name }}</h3>
+
+        <h3 v-if="info.title">Titolo: {{ info.title }}</h3>
+        <h3 v-else>Titolo: {{ info.name }}</h3>
+        <!-- /Info sui Film e Serie -->
+
+        <!-- Voto -->
+        <span class="stars" v-for="n in votoCalc" :key="n">
+          <i class="fas fa-star"></i>
+        </span>
+
+        <span class="starsEmpty" v-for="numero in 5 - votoCalc" :key="numero">
+          <i class="far fa-star"></i>
+        </span>
+        <!-- /Voto -->
+
+        <h4>Lingua:</h4>
+        <lang-flag :iso="info.original_language" />
       </div>
-        <div class="info-card">
-         <h3 v-if="info.original_title">
-        Titolo originale: {{ info.original_title }}
-      </h3>
-      <h3 v-else>Titolo originale: {{ info.original_name }}</h3>
-
-      <h3 v-if="info.title">Titolo: {{ info.title }}</h3>
-      <h3 v-else>Titolo: {{ info.name }}</h3>
-      <!-- /Info sui Film e Serie -->
-
-      <!-- Voto -->
-      <span class="stars" v-for="n in votoCalc" :key="n">
-        <i class="fas fa-star"></i>
-      </span>
-
-      <span class="starsEmpty" v-for="numero in 5 - votoCalc" :key="numero">
-        <i class="far fa-star"></i>
-      </span>
-      <!-- /Voto -->
-
-      <h4>Lingua:</h4>
-      <lang-flag :iso="info.original_language" />
-
-      </div>
-      <!-- Info sui Film e Serie -->
-      
-      <!--/Poster -->
     </div>
 
+    <!-- Info sui Film e Serie -->
+
+    <!--/Poster -->
+  </div>
 </template> 
 
 <script>
@@ -67,24 +73,23 @@ export default {
     display: inline-block;
     color: gold;
   }
-  .card-img{
+  .card-img {
     width: 11.25rem;
     height: 15.625rem;
     overflow: hidden;
-     img{
-       width: 100%;
-     }
+    img {
+      width: 100%;
+    }
   }
-  .info-card{
-    font-size: .9375rem;
+  .info-card {
+    font-size: 0.9375rem;
     border: 1px solid white;
     width: 11.25rem;
     height: 15.625rem;
     padding: 1.25rem;
   }
 }
-.card-img:hover .info-card{
-background-color: magenta;
+.card-img:hover img {
+  display: none;
 }
-
 </style>
